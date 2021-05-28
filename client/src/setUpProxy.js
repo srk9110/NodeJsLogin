8959c577564e63a952aa69d7315f05677443ca60
srk9110 <65576400+srk9110@users.noreply.github.com>
2021-05-28 18:22:17 +0900

    edit proxy2

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app){
     app.use(
         '/api',
         createProxyMiddleware ({
-            target:"ec2-54-180-135-68.ap-northeast-2.compute.amazonaws:5000",
+            target:"ec2-54-180-135-68.ap-northeast-2.compute.amazonaws.com:5000",
             changeOrigin: true,
         })
     );
